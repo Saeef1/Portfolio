effaba1c2a5d9c2369c6e128d6b04f642293ddbc
Saeef1 <muhammadsaeef045@gmail.com>
2025-07-27 00:19:49 +0500

    SEO friendly

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,11 +8,26 @@ import { ClerkProvider} from '@clerk/nextjs';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Muhammad Saeef Portfolio',
-  description: 'A showcase of my Next.js skills and projects',
+   title: 'Muhammad Saeef | Web Developer & Designer Portfolio',
+  description: 'Hi, I’m Muhammad Saeef — a creative web developer and designer specializing in modern, responsive websites using Next.js, React, and more.',
+  keywords: ['Muhammad Saeef', 'Web Developer', 'Frontend Developer', 'Next.js Portfolio', 'Creative Designer', 'Freelancer Pakistan'],
+  authors: [{ name: 'Muhammad Saeef' }],
   verification: {
     google: '85tj4GfcBCMItqW5s_6jxhvb8txI5lglxw7ia2CJ2yw',
-  }};
+  },
+  openGraph: {
+    title: 'Muhammad Saeef | Web Developer Portfolio',
+    description: 'Explore the projects and skills of Muhammad Saeef, a passionate web developer and designer.',
+    url: 'https://portfolio-nine-lemon-48.vercel.app',
+    siteName: 'Muhammad Saeef Portfolio',
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
 
 export default function RootLayout({
   children,
